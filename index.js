298f5da3875103b9de830aec7a2266c8afa0d228
bmo <bmo@bmo>
2022-10-03 09:31:17 -0600

    msg hello-world

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("My app") 
+const express = require("express")
+const app = express();
+const port = 3000;
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello My server")
+})
+
+app.get("/p", (req, res) => {
+  res.json({
+    name: "Product-1",
+    price: 1000
+
+  });
+});
+
+
+app.listen(port, () => {
+  console.log("Port: ", port)
+})
