45ecd81e60680e0defc9ab2d2e74f735aa960a46
bmo <bmo@bmo>
2022-10-03 12:59:24 -0600

    using params

--- index.js
+++ index.js
@@ -16,6 +16,43 @@ app.get("/p", (req, res) => {
   });
 });
 
+app.get("/ps", (req, res) => {
+  res.json([
+    {
+      name: "Product-1",
+      price: 1000
+
+    },
+    {
+      name: "Product-2",
+      price: 1000
+
+    },
+
+  ]);
+});
+
+
+app.get("/ps/:id", (req, res) => {
+  const {id } = req.params;
+
+  res.json({
+    id,
+    name:"product-2",
+    price: 200
+  });
+});
+
+app.get("/c/:idC/ps/:idP", (req, res) => {
+
+  const { idC, idP } = req.params;
+
+  res.json({
+    idC,
+    idP,
+  });
+});
+
 
 app.listen(port, () => {
   console.log("Port: ", port)
